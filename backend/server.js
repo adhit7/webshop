cab93e772a5cb042cea8af6051c11f7441919d99
adhit7 <adhithyanalan@gmail.com>
2024-08-04 20:54:25 +0530

    cors updates

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import cors from 'cors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -21,6 +22,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+app.use(cors({ credentials: true, origin: true }));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
